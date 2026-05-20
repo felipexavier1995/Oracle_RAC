9c8f54aaef4105aeebe2be1c28f85cd356673614
Felipe Bento Xavier <52262336+felipexavier1995@users.noreply.github.com>
2026-05-19 21:25:03 -0300

    Create validação.sql

---
+++ Oracle/validação.sql
@@ -0,0 +1,14 @@
+
+/*
+-- Query.
+
+alter session set nls_date_format='dd/mm/yy:hh24:mi:ss';
+set pagesize 5555
+set linesize 500
+col host_name for a20
+select sysdate, startup_time, host_name, instance_name, status, v.sessions_current, v.sessions_highwater
+from v$instance, v$license v
+/
+
+
+*/
